438ec9c988e771fc9f5b33032f1b335a49e48e00
abaume <baume.aurore@gmail.com>
2017-12-29 11:53:30 +0100

    rangement

--- ProjetWebBaumeLapicardise/Connexion.php
+++ ProjetWebBaumeLapicardise/Connexion.php
@@ -2,7 +2,10 @@
 <title>connexion</title>
 </head> 
 <?php include('/includes/menu.inc.php'); ?>
-
+<?php
+session_start();
+if ($id !=0) erreur(ERR_IS_CO);
+?>
   <div class="container-fluid">
     <h1>Connexion</h1>
     <form method="post" action="Traite_Connexion.php?url=%3C?php%20echo%20$_REQUEST[">
