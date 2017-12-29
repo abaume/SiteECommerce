1b0acb10e3bf9d196ed1f844893456688c4a4715
abaume <baume.aurore@gmail.com>
2017-12-29 12:54:31 +0100

    début enregistrement

--- Connexion.php
+++ Connexion.php
@@ -3,18 +3,34 @@
 </head> 
 <?php include('/includes/menu.inc.php'); ?>
 <?php
+$id=(isset($_SESSION['id']))?(int) $_SESSION['id']:0;
+$pseudo=(isset($_SESSION['pseudo']))?$_SESSION['pseudo']:'';
+
 session_start();
 if ($id !=0) erreur(ERR_IS_CO);
-?>
-  <div class="container-fluid">
-    <h1>Connexion</h1>
+
+include("./functions/erreur.php");
+include("./functions/constant.php");
+
+if (!isset($_POST['pseudo']))
+{
+  ?>
+   <div class="container-fluid">
     <form method="post" action="Traite_Connexion.php?url=%3C?php%20echo%20$_REQUEST[">
-	Nom : 
-    <input name="Login" type="text" />
-    <br />Mot de passe : 
-    <input name="Password" type="text" />
+	<fieldset>
+  <legend> <h1>Connexion</h1> </legend>
+  <p>
+  <label for="pseudo"> Pseudo :</label><input name="pseudo" type="text" id="pseudo"/>
     <br />
+  <label for="password"> Mot de passe : </label><input name="Password" type="text" id="password"/>
+    </p>
+    </fieldset>
+    <p>
     <input name="Connect" type="submit" value="Connecter" /></form>
+    <a href="/register.php"> Pas encore inscrit ?</a>
 	</div>
+  <?php
+}
+?>
   </body>
 </html>
--- functions/constant.php
+++ functions/constant.php
@@ -1,3 +1,5 @@
 <?php
+define('VISITEUR',1);
+define('INSCRIT',2);
 define('ERR_IS_CO','Vous ne pouvez pas accéder à cette page si vous n\'êtes pas connecté');
 ?>
