f56ad7ddbfcc6a2a0d115682014408f0652e560b
abaume <baume.aurore@gmail.com>
2017-12-29 11:34:33 +0100

    connexion pour acceder au panier

--- ProjetWebBaumeLapicardise/panier.php
+++ ProjetWebBaumeLapicardise/panier.php
@@ -1,14 +1,21 @@
+<?php
+session_start();
+if (isset($_SESSION["NOM_USER"]))
+{
+	echo "<p> voici votre panier ".$_SESSION["NOM_USER"]. "</p>";
+}
+else
+{
+	$url = $_SERVER["REQUEST_URI"];
+	header("Location: Connexion.php?url=".$url);
+}
+?>
 <?php include('/includes/head.inc.php'); ?>
-    <title>Accueil</title>
+    <title>Panier</title>
   </head>
   <?php include('/includes/menu.inc.php'); ?>
 	<div class="container-fluid">
-    <h1>Travail réalisé</h1>
-    <p>Projet Web réalisé par Baume Aurore et Lapicardise Romain.
-    <br />Il s'agit de construire un site web de type e-commerce s'appuyant sur la base de données de musique
-    Classique_Web disponible sur le serveur info-simplet.</p>
-    <h1>Difficultés rencontrées</h1>
-    <p>oulala</p>
+    <p> voici votre panier </p>
 	</div>
   </body>
 </html>
