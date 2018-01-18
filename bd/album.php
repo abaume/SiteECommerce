db2a4f1ef09be01b2f9c14b5f194d1cd9fd4a6df
Lapicardise Romain <romain.lapicardise@etu.u-bordeaux.fr>
2018-01-18 09:37:57 +0100

    avancement

--- bd/album.php
+++ bd/album.php
@@ -47,29 +47,41 @@ if (!empty($_GET["code"])) {
 	foreach ($pdo->query($requete) as $row) {
 		echo 
 		"<h4>". $row['Titre'] . "</h4>" .
-		"<p><audio src=\"/Classique/Home/Extrait/" . $row['Code_Morceau'] . "\" controls></audio>" .
+		"<form method=\"get\"><p><audio src=\"/Classique/Home/Extrait/" . $row['Code_Morceau'] . "\" controls></audio>" .
 		"(" . $row[utf8_decode('Durée')] . ")" .
 		"<span class=\"btn\"><input type=\"submit\" value=\"Ajouter\" name=\"ajout\" class=" .
-		$row['Code_Morceau'] . "</span></p>";
+		$row['Code_Morceau'] . "</span></p></form>";
 	}
-	}
-	if (!empty($_GET["ajout"])) {
-		$ajout = $_GET["ajout"];
-		echo "Ajout...";
-
-		$requete = "SELECT Code_Achat, Code_Enregistrement, Code_Abonné FROM Achat
-		INNER JOIN Composition_Disque On Composition_Disque.Code_Morceau = Achat.Code_Enregistrement
-		INNER JOIN Disque On Disque.Code_Disque = Composition_Disque.Code_Disque
-		INNER JOIN Album ON Disque.Code_Album = Album.Code_Album
-		INNER JOIN Abonné ON Abonné.Code_Abonné = Achat.Code_Abonné ";
-		  $buffer = $pdo->query($requete);
-		  foreach ($pdo->query($requete) as $row) {
-			  $requete = "INSERT INTO Achat(Code_Achat, Code_Enregistrement, Code_Abonné) 
-			  VALUES(\'' . $Code_Achat . '\', \'' . $Code_Enregistrement . '\', \'' . $Code_Abonné . '\') ";
-		  }
-		echo "Ajouté au panier !";
-	$pdo = null;
 }
+if (!empty($_GET["ajout"])) {
+		$ajout = $_GET["ajout"];
+		$login = $_SESSION["Login"];		
+		echo "Ajout...";
+		
+		$requete = "SELECT Code_Abonné From Abonné Where Login = \"" . $login . "\"";		
+		$buffer = $pdo->query($requete);
+		
+		foreach ($pdo->query($requete) as $row) {
+			
+			$requete2 = "INSERT INTO Achat(Code_Enregistrement, Code_Abonné) 
+			  VALUES(\'' . 
+			  $ajout . 
+			  '\', '" . 
+			  $row['$Code_Abonné'] . 
+			  ")";		  
+			  
+		// SELECT Code_Achat, Code_Enregistrement, Achat.Code_Abonné FROM Achat
+		// INNER JOIN Composition_Disque On Composition_Disque.Code_Morceau = Achat.Code_Enregistrement
+		// INNER JOIN Disque On Disque.Code_Disque = Composition_Disque.Code_Disque
+		// INNER JOIN Album ON Disque.Code_Album = Album.Code_Album
+		// INNER JOIN Abonné ON Abonné.Code_Abonné = Achat.Code_Abonné ";
+		
+		  $buffer = $pdo->query($requete2);			
+		}
+		
+		echo "Ajouté au panier !";
+}
+		$pdo = null;
 ?>
 </p>
 </div>
