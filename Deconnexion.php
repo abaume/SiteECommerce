9732191200c4c61e397981e1ba0456750eca67cf
Lapicardise Romain <romain.lapicardise@etu.u-bordeaux.fr>
2018-01-21 21:32:44 +0100

    indentation

--- Deconnexion.php
+++ Deconnexion.php
@@ -1,21 +1,22 @@
 <?php include('/includes/head.inc.php'); ?>
-<title>deconnexion</title>
-</head> 
-<?php 
-session_start();
-$_SESSION = array();
+    <title>deconnexion</title>
+  </head><?php 
+  session_start();
+  $_SESSION = array();
 
-if (isset ($_COOKIE['Login'])) {
-    setcookie('Login', '', -1);
-}
+  if (isset ($_COOKIE['Login'])) {
+      setcookie('Login', '', -1);
+  }
 
-session_destroy();
-$titre="Déconnexion";
-include('/includes/menu.inc.php'); 
+  session_destroy();
+  $titre="Déconnexion";
+  include('/includes/menu.inc.php'); 
 
-echo '<p>Vous êtes à présent déconnecté <br />
-Cliquez <a href="'.htmlspecialchars($_SERVER['HTTP_REFERER']).'">ici</a> 
-pour revenir à la page précédente.<br />
-Cliquez <a href="./index.php">ici</a> pour revenir à la page principale</p>';
-echo '</div></body></html>';
-?>
+  echo '<p>Vous êtes à présent déconnecté <br />
+  Cliquez <a href="'.htmlspecialchars($_SERVER['HTTP_REFERER']).'">ici</a> 
+  pour revenir à la page précédente.<br />
+  Cliquez <a href="./index.php">ici</a> pour revenir à la page principale</p>';
+  echo '</div></body></html>';
+  ?>
+  <body></body>
+</html>
--- Traite_Connexion.php
+++ Traite_Connexion.php
@@ -2,48 +2,47 @@
     require('/functions/valide.php');
     include('includes/identifiants.php');
     include('/includes/head.inc.php'); ?>
-<title>connexion</title>
-</head> 
-<?php
-include('/includes/menu.inc.php'); 
+    <title>connexion</title>
+  </head><?php
+  include('/includes/menu.inc.php'); 
 
-    $message='';
-    $url = $_REQUEST['url'];
+      $message='';
+      $url = $_REQUEST['url'];
 
 
-    if (empty($_REQUEST['Login']) || empty($_REQUEST['Password']) ) //Oublie d'un champ
-    {
-        $message = '<p>une erreur s\'est produite pendant votre identification.
-	Vous devez remplir tous les champs</p>
-	<p>Cliquez <a href="./connexion.php">ici</a> pour revenir</p>';
-    } else {
+      if (empty($_REQUEST['Login']) || empty($_REQUEST['Password']) ) //Oublie d'un champ
+      {
+          $message = '<p>une erreur s\'est produite pendant votre identification.
+          Vous devez remplir tous les champs</p>
+          <p>Cliquez <a href="./connexion.php">ici</a> pour revenir</p>';
+      } else {
 
-    $login = $_REQUEST['Login'];
-    $rep = $pdo->prepare('SELECT * FROM Abonné WHERE Login= :log');
-    $rep->execute(array(
-        'log' => $login));
-    $data = $rep->fetch();
+      $login = $_REQUEST['Login'];
+      $rep = $pdo->prepare('SELECT * FROM Abonné WHERE Login= :log');
+      $rep->execute(array(
+          'log' => $login));
+      $data = $rep->fetch();
 
-        if ($data['Password'] == $_REQUEST['Password']) {
-            if (isset($_REQUEST['souvenir'])) {
-                $expire = time() + 365*24*3600;
-                setcookie('Login', $_SESSION['Login'], $expire);
-            }
-            session_start();
-            $_SESSION['Login'] = $data['Login'];
-            $message = '<p>Bienvenue '.$data['Login'].', 
-			vous êtes maintenant connecté!</p>
-			<p>Cliquez <a href="' . $url . '">ici</a> 
-			pour revenir à la page précédente</p>';  
-        } else {
-            $message = '<p>Une erreur s\'est produite 
-            pendant votre identification.<br /> Le mot de passe ou le pseudo 
-                entré n\'est pas correct.</p><p>Cliquez <a href="'. $url . '">ici</a> 
-            pour revenir à la page précédente';
-        }
-        $rep->CloseCursor();
-    }
-    echo $message.'</div></body></html>';
-?>
-</body>
+          if ($data['Password'] == $_REQUEST['Password']) {
+              if (isset($_REQUEST['souvenir'])) {
+                  $expire = time() + 365*24*3600;
+                  setcookie('Login', $_SESSION['Login'], $expire);
+              }
+              session_start();
+              $_SESSION['Login'] = $data['Login'];
+              $message = '<p>Bienvenue '.$data['Login'].', 
+                          vous êtes maintenant connecté!</p>
+                          <p>Cliquez <a href="' . $url . '">ici</a> 
+                          pour revenir à la page précédente</p>';  
+          } else {
+              $message = '<p>Une erreur s\'est produite 
+              pendant votre identification.<br /> Le mot de passe ou le pseudo 
+                  entré n\'est pas correct.</p><p>Cliquez <a href="'. $url . '">ici</a> 
+              pour revenir à la page précédente';
+          }
+          $rep->CloseCursor();
+      }
+      echo $message.'</div></body></html>';
+  ?>
+  <body></body>
 </html>
