8235622dad3cf504e54f663b5963a67427c674d5
Lapicardise Romain <romain.lapicardise@etu.u-bordeaux.fr>
2018-01-21 18:38:41 +0100

    blabla

--- apropos.php
+++ apropos.php
@@ -5,13 +5,18 @@
 	<div class="container-fluid">
     <h1>Travail réalisé</h1>
     <p>Projet Web réalisé par Baume Aurore et Lapicardise Romain.
-    <br />Il s&#39;agit de construire un site web de type e-commerce s&#39;appuyant sur la base de données de musique
-    Classique_Web disponible sur le serveur info-simplet.</p>
+    <br>Il s'agit de construire un site web de type e-commerce s'appuyant sur la base de données de musique Classique_Web disponible sur le serveur info-simplet.</p>
 	</div>
 	<div class="container-fluid">
     <h1>Difficultés rencontrées</h1>
-    <p>Le site prévoyait d'être en symfony mais des erreurs répétées nous ont contraints à ne le développer 
-    qu'en php pur. </p>
+    <p><ul>
+	<li>Le site prévoyait d'être en Symfony mais des erreurs répétées nous ont contraints à ne le développer 
+    qu'en php pur. </li>
+	<li>L'utilisation et l'intégration de l'API Amazon a été complexe d'une part par la difficulté de créer un compte et récupérer les bonnes "clés" mais aussi par le manque d'informations sur comment traiter et afficher la réponse d'Amazon. Aussi, le fait d'afficher des albums semblables a pris beaucoup de temps</li>
+	<li>Le serveur info-timide qui était instable durant les derniers jours.</li>
+	<li>Besoin que se documenter pour réaliser la plupart des notions.</li>
+	<li>Travailler en local n'a pas été pratique pour l'utilisation de la base de données, mais nous sommes passé outre.</li>
+	</ul></p>
 	</div>
   </body>
 </html>
--- index.php
+++ index.php
@@ -9,25 +9,21 @@
 <div class="container-fluid row">
   <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9 ">
     <h1>Présentation du site</h1>
-    <p>Bienvenue sur la page d&#39;accueil de ce magnifique site web e-commerce !</p>
+    <p>Bienvenue sur la page d'accueil de ce magnifique site web e-commerce !</p>
   </div>
   <br />
   <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
   <h3>Ce site est divisé en plusieurs parties :</h3>
   <ul>
-    <li>Une page d&#39;accueil présentant le site.</li>
+    <li>Cette page d'accueil présentant le site.</li>
     <li>Un menu facilitant la navigation dans le site.</li>
-    <li>Une page &quot;A propos&quot; décrivant le travail réalisé et précisant la liste des auteurs du site (notre binôme),
+    <li>Une page "A propos" décrivant le travail réalisé et précisant la liste des auteurs du site (notre binôme),
     et nos difficultés rencontrées.</li>
-    <li>Un ensemble de pages constituant un catalogue et permettant de parcourir le contenu de la base (par exemple : un lien qui
-    à partir d&#39;une initiale permet d&#39;accéder aux oeuvres d&#39;un compositeur, puis aux albums contenant des
-    enregistrements de ces oeuvres, et enfin aux enregistrement eux-mêmes).</li>
-    <li>Chaque fois que c&#39;est pertinent, affichage de la photo des musiciens ou de la pochette d&#39;un album, affichage
-    d&#39;un contrôle permettant d&#39;écouter l&#39;extrait de l&#39;enregistrement concerné.</li>
-    <li>Depuis la page décrivant un album, un accès aux services Amazon affichant les informations sur l&#39;album (détails,
-    prix, ...) grâce à la valeur contenue dans le champ ASIN de la table Album (en utilisant l&#39;API Amazon ), ou la liste des
-    albums semblables disponibles lorsque ce champ n&#39;est pas renseigné (pas un simple renvoi sur une page !).</li>
-    <li>Une zone sécurisée (donc avec connexion et suivi de session) permettant de construire un panier d&#39;achat.</li>
+    <li>Un ensemble de pages accessible depuis l'onglet "Albums", constituant un catalogue et permettant de parcourir le contenu de la base. Par exemple : à partir d'une initiale ou du début du nom d'un compositeur, on peut accéder à ses albums contenant des enregistrements de ses oeuvres, et enfin pouvoir les écouter ou bien même avoir des informations proposées par Amazon.</li>
+    <li>Chaque fois que c'est pertinent, un affichage d'une photo des musiciens ou de la pochette d'un album ainsi qu'un contrôle permettant d'écouter l'extrait de l'enregistrement concerné.</li>
+    <li>Depuis la page décrivant un album, un accès aux services Amazon affichant les informations sur l'album (détails, prix, ...), ou la liste des
+    albums semblables disponibles lorsque qu'une correspondance n'a pu être faite.</li>
+    <li>Une zone sécurisée (donc avec connexion et suivi de session) permettant de construire un panier d'achat, de le vider partiellement ou totalement et de confirmer celui-ci ; mais aussi afficher les 5 derniers albums commandés</li>
   </div>
 </div>
   </body>
