1d6b1e78b978a936e69c438512ae4e5cd10b93c9
Lapicardise Romain <romain.lapicardise@etu.u-bordeaux.fr>
2018-01-21 17:12:41 +0100

    navbar parfait

--- includes/navbarBD.inc.php
+++ includes/navbarBD.inc.php
@@ -1,22 +1,23 @@
-      <ul class="navbar">
+<div class="navbar">
+      <ul>
         <li>
           <a href = "baseIndex.php">Accueil</a>
         </li>
         <li class="dropdown">
           <a href="#listeAlpha" class="dropbtn">Liste alphabétique</a>
-          <ul class="dropdown-content">
-          <li><a href="compositeur.php">Compositeurs</a></li>
-          <li><a href="interprete.php">Interprètes</a></li>
-          <li><a href="chef.php">Chefs d'orchestre</a></li>
-          <li><a href="orchestre.php">Orchestres</a></li>
-          </ul>
+		  <div class="dropdown-content">
+			  <a href="compositeur.php">Compositeurs</a>
+			  <a href="interprete.php">Interprètes</a>
+			  <a href="chef.php">Chefs d'orchestre</a>
+			  <a href="orchestre.php">Orchestres</a>
+			</div>
         </li>
         <li class="dropdown">
           <a href="#epoque" class="dropbtn">Par époques</a>
-          <ul class="dropdown-content">
-			        <li><a href="compositeurEpoque.php">Compositeurs</a></li>
-			        <li><a href="interpreteEpoque.php">Interprètes</a></li>
-          </ul>
+		  <div class="dropdown-content">
+			  <a href="compositeurEpoque.php">Compositeurs</a>
+			  <a href="interpreteEpoque.php">Interprètes</a>
+			</div>
         </li>
         <li>
           <a href="instrument.php">Instruments</a>
@@ -25,3 +26,4 @@
           <a href="genre.php">Genres</a>
         </li>
       </ul>
+    </div>
